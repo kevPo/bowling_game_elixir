7f7c93caa7c2cb98114530ac39116a4291695aa6
Kevin Poston <kevposton@gmail.com>
2021-06-06 16:10:42 -0400

    picked a pattern to go with

--- lib/bowling_game.ex
+++ lib/bowling_game.ex
@@ -1,43 +1,20 @@
 defmodule BowlingGame do
-  def calculate(rolls) do
-    calculate(rolls, 0)
+  def get_score(rolls), do: get_score(rolls, 0)
+  defp get_score([], score), do: score
+
+  defp get_score([first, second, third | []], score) do
+    first + second + third + score
   end
 
-  defp calculate([], score) do
-    score
+  defp get_score([10, second, third | rolls], score) do
+    get_score([second, third] ++ rolls, score + 10 + second + third)
   end
 
-  defp calculate([roll, second, third | []], score) do
-    score + roll + second + third
+  defp get_score([first, second, third | rolls], score) when first + second == 10 do
+    get_score([third] ++ rolls, score + 10 + third)
   end
 
-  defp calculate([10, second, third | rolls], score) do
-    calculate([second, third] ++ rolls, score + 10 + second + third)
-  end
-
-  defp calculate([first, second, third | rolls], score) when first + second == 10 do
-    calculate([third] ++ rolls, score + 10 + third)
-  end
-
-  defp calculate([first, second | rolls], score) do
-    calculate(rolls, score + first + second)
-  end
-
-  def get_score(rolls) do
-    get_score(rolls, [])
-  end
-
-  defp get_score([x, y], frameScores) do
-    (frameScores ++ [x + y])
-    |> Enum.slice(0, 10)
-    |> Enum.sum()
-  end
-
-  defp get_score([firstRoll, secondRoll, thirdRoll | tail], frameScores) do
-    case {firstRoll, secondRoll, thirdRoll} do
-      {10, _, _} -> get_score([secondRoll, thirdRoll] ++ tail, frameScores ++ [10 + secondRoll + thirdRoll])
-      {x, y, _} when x + y == 10 -> get_score([thirdRoll] ++ tail, frameScores ++ [10 + thirdRoll])
-      {_, _, _} -> get_score([thirdRoll] ++ tail, frameScores ++ [firstRoll + secondRoll])
-    end
+  defp get_score([first, second | rolls], score) do
+    get_score(rolls, score + first + second)
   end
 end
--- test/bowling_game_test.exs
+++ test/bowling_game_test.exs
@@ -5,31 +5,26 @@ defmodule BowlingGameTest do
   test "all gutters returns 0" do
     rolls = replicate(20, 0)
     assert BowlingGame.get_score(rolls) == 0
-    assert BowlingGame.calculate(rolls) == 0
   end
 
   test "non-marking returns sum" do
     rolls = replicate(20, 1)
     assert BowlingGame.get_score(rolls) == 20
-    assert BowlingGame.calculate(rolls) == 20
   end
 
   test "spare adds bonus" do
     rolls = [3, 7, 3] ++ replicate(17, 0)
     assert BowlingGame.get_score(rolls) == 16
-    assert BowlingGame.calculate(rolls) == 16
   end
 
   test "strike adds bonus" do
     rolls = [10, 2, 2] ++ replicate(16, 0)
     assert BowlingGame.get_score(rolls) == 18
-    assert BowlingGame.calculate(rolls) == 18
   end
 
   test "perfect game" do
     rolls = replicate(12, 10)
     assert BowlingGame.get_score(rolls) == 300
-    assert BowlingGame.calculate(rolls) == 300
   end
 
   def replicate(n, x), do: for _ <- 1..n, do: x

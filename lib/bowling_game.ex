85c8af2859edb5e1e4bc2f3d036149cd88d06056
Kevin Poston <kevposton@gmail.com>
2021-06-05 20:02:57 -0400

    made a few changes

--- lib/bowling_game.ex
+++ lib/bowling_game.ex
@@ -27,19 +27,17 @@ defmodule BowlingGame do
     get_score(rolls, [])
   end
 
-  def get_score([x, y], frames) do
-    (frames ++ [x + y])
+  defp get_score([x, y], frameScores) do
+    (frameScores ++ [x + y])
     |> Enum.slice(0, 10)
     |> Enum.sum()
   end
 
-  def get_score([head | tail], frames) do
-    [firstRoll | [secondRoll | [thirdRoll | _]]] = [head | tail]
-
+  defp get_score([firstRoll, secondRoll, thirdRoll | tail], frameScores) do
     case {firstRoll, secondRoll, thirdRoll} do
-      {10, _, _} -> get_score(tail, frames ++ [10 + secondRoll + thirdRoll])
-      {x, y, _} when x + y == 10 -> get_score(Enum.drop(tail, 1), frames ++ [10 + thirdRoll])
-      {_, _, _} -> get_score(Enum.drop(tail, 1), frames ++ [firstRoll + secondRoll])
+      {10, _, _} -> get_score([secondRoll, thirdRoll] ++ tail, frameScores ++ [10 + secondRoll + thirdRoll])
+      {x, y, _} when x + y == 10 -> get_score([thirdRoll] ++ tail, frameScores ++ [10 + thirdRoll])
+      {_, _, _} -> get_score([thirdRoll] ++ tail, frameScores ++ [firstRoll + secondRoll])
     end
   end
 end
